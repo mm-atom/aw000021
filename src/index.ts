a25d8b665300c9fc51c351b948762454f7db0c98
taoqf <tao_qf@163.com>
2020-03-16 12:54:45 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
+import global from '@mmstudio/global';
 
-export default async function aw21(param1: string) {
+/**
+ * 根据文件名删除文件服务器上的文件
+ * @param productid 产品id
+ * @param file_id 要删除的文件id,上传时返回的filename
+ */
+export default async function remove(file_id: string[]) {
+	try {
+		const res = await fetch(`${global('host', '.')}/fsweb/delfile?id=${file_id.join(',')}`, {
+			method: 'POST'
+		});
+		const ret = await res.json() as { code: 0 | 1; code_msg: '删除成功！' | string };
+		return ret && ret.code === 1;
+	} catch (error) {
+		// console.error(error);
+		return false;
+	}
 }
--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,10 @@
 import { expect } from 'chai';
 import t from '../dist/index';
 
-describe('xxx', () => {
-	it('xxx', () => {
-		t();
-		expect(1).not.eq(2);
+describe('remove file', () => {
+	it('remove file by id', async () => {
+		window.host = 'http://127.0.0.1:8889';
+		const v = await t(['6016884b-41d3-4157-9cf5-730203c6ea5c']);
+		expect(v).eq(true);
 	});
 });
